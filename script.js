d1fbaea303a3e38c6d884e8b8ffccd22f64be958
abdiazizjamal34 <151532198+abdiazizjamal34@users.noreply.github.com>
2023-12-02 08:11:00 +0300

    add file via uploded
    js

---
+++ script.js
@@ -0,0 +1,18 @@
+var age = 125;
+ console.log(age);
+
+
+ 
+ const m  = window.prompt("what is your mass?");
+
+console.log(m);
+
+const h= window.prompt(" Enter your height");
+console.log(h);
+ const result = m/(h*h);
+
+ console.log(result);
+ for(let i = 0; i<result; i++){
+    console.log(" bad");
+ }
+
